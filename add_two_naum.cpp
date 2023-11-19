33034aa4c2f6de5c8e461e0727d02f00e3deaf8a
Ranjan Kumar <vtu16964@veltech.edu.in>
2023-11-20 03:17:53 +0530

    Improved readability with added comment

---
+++ add_two_naum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //declaration of two integer variable
+    int a;
+    int b;
+
+    //Asking input from user
+    cout<<"Enter the the value of a: ";
+    cin>>a;
+    cout<<"Enter the the value of b: ";
+    cin>>b;
+
+    // performing operation
+    int c = a+b;
+
+    //prints the result
+    cout<<"the sum of "<< a <<" and "<< b << " is "<< c <<endl;
+    return 0;
+
+}
