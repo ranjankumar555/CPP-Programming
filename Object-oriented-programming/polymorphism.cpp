9de175a0cdac8c5c290208424adf9fda08e2f942
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-01-13 13:45:55 +0530

    Polymorphism Concept

---
+++ Object-oriented-programming/polymorphism.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class A{
+  private:
+  int a;
+  public:
+  void display(){
+    cout<<"Class A invoked"<<endl;
+  }
+};
+
+class B: public A{
+  private:
+  int b;
+  public:
+  void display(){
+    cout<<"Class B invoked"<<endl;
+  }
+};
+
+int main(){
+  B obj;
+  obj.display();
+  return 0;
+}
