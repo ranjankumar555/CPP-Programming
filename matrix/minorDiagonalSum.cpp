2316521d203dd1d7965b9fddd63fd7f01d6f35f2
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-02-07 23:51:22 +0530

    minorDiagonalSum.cpp

---
+++ matrix/minorDiagonalSum.cpp
@@ -0,0 +1,28 @@
+/*Write a C program to find sum of Minor diagonal elements of a matrix.
+Concept: minor diagonal of a matrix can be mathematically represented as 
+i+j = N+1; 
+where N is the order of matrix
+*/
+#include<iostream>
+using namespace std;
+#define SIZE 10
+int diagonalSum(int m[SIZE][SIZE], int order){
+  int sum=0;
+  for(int i=0;i<order;i++){
+    for(int j=0;j<order;j++){
+      if(i+j==(order+1)-2){ // since array indexing starts from 0; therefore need to substract 1 to main relation.
+        sum+=m[i][j];
+      }      
+    }
+  }
+  return sum;
+}
+int main(){
+  int order;
+  int m[SIZE][SIZE]={{1,2,3},{4,5,6},{7,8,9}}; // valid initialisation
+  // int m[SIZE][SIZE]={1,2,3,4,5,6,7,8,9}; / invalid initialisation produce unexpected result
+  cout<<"Enter the order of matrix: ";
+  cin>>order;
+  cout<<"The sum of minor diagonal element of matrix: "<<diagonalSum(m,order);
+  return 0;
+}
