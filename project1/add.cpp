3a0b77561158945ab714457f39b1c31f2a1891f3
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-09-09 16:34:19 +0530

    Function call from other separate file

---
+++ project1/add.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include "add.h"
+int addFunction(int a, int b){
+  return a+b;
+}
---
+++ project1/main.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+#include<iostream>
+using namespace std;
+int main(){
+  int a=5;
+  int b=6;
+  int c = addFunction(a,b);
+  cout<<"The sum of two number is: "<<c<<endl;
+  return 0;
+}
